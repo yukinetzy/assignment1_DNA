4a0cc4ce4a6afea827257c3783c6ad63908029c8
yukinetzy <meliodasusai@gmail.com>
2025-09-27 18:13:34 +0500

    init: maven quicksort project

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+}
